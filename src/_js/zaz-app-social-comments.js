c5fada5fba5cb1267db5d24bae1f381c71a51322
vitor.barbosa <vbarbosa.canopus@terra.com.br>
2014-10-08 23:43:33 +0000

    ação do app de comentarios usando a ferramenta disqus

--- src/_js/zaz-app-social-comments.js
+++ src/_js/zaz-app-social-comments.js
@@ -96,6 +96,21 @@ zaz.use(function appSocialComments(pkg) {
                 var PUBLIC = this,
                     PRIVATE = {};
 
+                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                var disqus_shortname = 'vbarbos'; // required: replace example with your forum shortname
+
+                this.createEmbed = function (disqus_shortname) {
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                };
+
+                this.createCounter = function (disqus_shortname) {
+                    var s = document.createElement('script'); s.async = true;
+                    s.type = 'text/javascript';
+                    s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+                    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+                };
 
                 /**
                  * Do something.
@@ -108,14 +123,15 @@ zaz.use(function appSocialComments(pkg) {
                  * @return {SocialCommentsInstance}
                  * @chainable Returns the same instance object, therefore, it's chainable
                  */
-                this.doSomething = function (first, second) {
+                /*this.doSomething = function (first, second) {
                     // do something
                     return this;
-                };
+                };*/
 
                 // for example, you know that you received a "someProp", once
                 // it was in your expected property
-                this.set('someProp', data.someProp);
+                this.createEmbed(disqus_shortname);
+                this.createCounter(disqus_shortname);
 
 
                 return PUBLIC;
