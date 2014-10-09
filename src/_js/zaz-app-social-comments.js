f718700bf66b2b52f14f59556725db1167144b88
vitor.barbosa <vbarbosa.canopus@terra.com.br>
2014-10-09 21:04:24 +0000

    alterações referentes ao lint. a variável disqus_shortname não pode ser alterada

--- src/_js/zaz-app-social-comments.js
+++ src/_js/zaz-app-social-comments.js
@@ -97,16 +97,19 @@ zaz.use(function appSocialComments(pkg) {
                     PRIVATE = {};
 
                 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                //var disqus_shortname = 'vbarbos'; // required: replace example with your forum shortname
+                var disqus_shortname = 'terranetworks'; // required: replace example with your forum shortname
 
                 this.createEmbed = function (disqus_shortname) {
-                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    var dsq = document.createElement('script');
+                    dsq.type = 'text/javascript';
+                    dsq.async = true;
                     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
                     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
                 };
 
                 this.createCounter = function (disqus_shortname) {
-                    var s = document.createElement('script'); s.async = true;
+                    var s = document.createElement('script');
+                    s.async = true;
                     s.type = 'text/javascript';
                     s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
                     (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
